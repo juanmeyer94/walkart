1cc3067d769ad67f0eef7e2624d7f7fe785926e6
IvoSoria <ivosoria@hotmail.com>
2023-08-30 12:19:42 -0300

    mejoras en Models

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/home";
 import { Button } from "@/components/ui/button"
 import NavBar from "./components/NavBar/navbar"
+import Models from "./components/Models/Models";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/models" element={<Models />} />
         
       </Routes>
     </div>
