72c7a902b5b042c6a172cd42805f8a2dd11d05a3
IvoSoria <ivosoria@hotmail.com>
2023-08-30 16:44:38 -0300

    Hardcodeo una api de modelos de zapas

--- src/components/Models/Models.jsx
+++ src/components/Models/Models.jsx
@@ -1,60 +1,94 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const modelos = [
+  {
+    name: "Dra",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+    description: "Una zapatilla de estilo único y moderno.",
+    category: "Running",
+  },
+  {
+    name: "ZapatoX",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+    description: "Un zapato elegante para ocasiones especiales.",
+    category: "Formal",
+  },
+  {
+    name: "AirMax",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+    description: "Las AirMax te brindan comodidad y estilo en cada paso.",
+    category: "Running",
+  },
+  {
+    name: "UrbanFlex",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+    description: "Las UrbanFlex son perfectas para la vida urbana y activa.",
+    category: "Casual",
+  },
+  {
+    name: "Elegance",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+    description: "Elegancia y estilo se fusionan en estas zapatillas.",
+    category: "Formal",
+  },
+  {
+    name: "ActiveStep",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+    description: "ActiveStep te acompaña en tus aventuras al aire libre.",
+    category: "Outdoor",
+  },
+  {
+    name: "ColorBurst",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+    description: "ColorBurst añade un toque vibrante a tu estilo diario.",
+    category: "Casual",
+  },
+  {
+    name: "SkyRide",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+    description: "SkyRide: la comodidad elevada a un nuevo nivel.",
+    category: "Running",
+  },
+  {
+    name: "WaveGlide",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+    description: "WaveGlide: zapatillas que te hacen sentir en las nubes.",
+    category: "Running",
+  },
+  {
+    name: "TrekPro",
+    image: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+    description: "TrekPro: listas para conquistar cualquier sendero.",
+    category: "Outdoor",
+  },
+];
+
 const Models = () => {
-
-
-    return (
-<div>
-
-<div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
-    <button type="button" className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">All categories</button>
-    <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Shoes</button>
-    <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Bags</button>
-    <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Electronics</button>
-    <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Gaming</button>
-</div>
-<div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+  return (
     <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
+      <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
+        {modelos.map((modelo, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800`}
+          >
+            {modelo.name}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {modelos.map((modelo, index) => (
+          <div key={index}>
+            <img className="h-auto max-w-full rounded-lg" src={modelo.image} alt={modelo.name} />
+            <p className="mt-2">{modelo.description}</p>
+            <p className="text-gray-600">{modelo.category}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt=""/>
-    </div>
-</div>
-</div>
-
-);
+  );
 };
 
 export default Models;
