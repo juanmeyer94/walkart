355d92a476dd3c88fbadb434c4118a0e1f925373
IvoSoria <ivosoria@hotmail.com>
2023-08-31 15:29:11 -0300

    Agregué el Modal

--- src/components/Models/Models.jsx
+++ src/components/Models/Models.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
 const modelos = [
   {
@@ -65,52 +64,176 @@ const modelos = [
 ];
 
 const Models = () => {
-    const [hoveredModel, setHoveredModel] = useState(null);
-    const [selectedCategory, setSelectedCategory] = useState("Todos");
-  
-    const uniqueCategories = ["Todos", ...new Set(modelos.map((modelo) => modelo.category))];
-  
-    const filteredModelos = selectedCategory === "Todos" ? modelos : modelos.filter((modelo) => modelo.category === selectedCategory);
-  
-    return (
-      <div>
-        <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
-          {uniqueCategories.map((category, index) => (
-            <button
-              key={index}
-              type="button"
-              className={`text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800`}
-              onClick={() => setSelectedCategory(category)}
-            >
-              {category}
-            </button>
-          ))}
-        </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          {filteredModelos.map((modelo, index) => (
-            <div
-              key={index}
-              className="relative"
-              onMouseEnter={() => setHoveredModel(index)}
-              onMouseLeave={() => setHoveredModel(null)}
-            >
-              <img
-                className={`h-auto max-w-full rounded-lg transition-opacity ${
-                  hoveredModel === index ? "opacity-75" : "opacity-100"
+  const [hoveredModel, setHoveredModel] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState("Todos");
+  const [selectedModel, setSelectedModel] = useState(null);
+
+  const uniqueCategories = ["Todos", ...new Set(modelos.map((modelo) => modelo.category))];
+
+  const filteredModelos = selectedCategory === "Todos" ? modelos : modelos.filter((modelo) => modelo.category === selectedCategory);
+
+  const handleModelClick = (index) => {
+    setSelectedModel(index);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedModel(null);
+  };
+
+  return (
+    <div>
+      <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
+        {uniqueCategories.map((category, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800`}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {filteredModelos.map((modelo, index) => (
+          <div
+            key={index}
+            className="relative cursor-pointer"
+            onMouseEnter={() => setHoveredModel(index)}
+            onMouseLeave={() => setHoveredModel(null)}
+            onClick={() => handleModelClick(index)}
+          >
+            <img
+              className={`h-auto max-w-full rounded-lg transition-opacity ${hoveredModel === index ? "opacity-75" : "opacity-100"
                 }`}
-                src={modelo.image}
-                alt={modelo.name}
-              />
-              {hoveredModel === index && (
-                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 text-white rounded-lg">
-                  {modelo.name}
-                </div>
-              )}
-            </div>
-          ))}
+              src={modelo.image}
+              alt={modelo.name}
+            />
+            {hoveredModel === index && (
+              <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-75 text-white rounded-lg">
+                {modelo.name}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+
+      //*MODAL*//
+      
+      {selectedModel !== null && (
+  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="bg-white rounded-lg shadow-lg w-full md:w-3/4 lg:w-2/3 xl:w-1/2 flex">
+      <div className="p-4 flex-shrink-0">
+        <img
+          src={filteredModelos[selectedModel].image}
+          alt={filteredModelos[selectedModel].name}
+          className="w-32 h-32 mx-auto mb-4"
+        />
+        <h3 className="text-2xl font-bold mb-2">
+          {filteredModelos[selectedModel].name}
+        </h3>
+        <p className="mb-4 text-gray-600">
+          {filteredModelos[selectedModel].category}
+        </p>
+        <p>{filteredModelos[selectedModel].description}</p>
+      </div>
+      <div className="p-4 flex-grow">
+
+
+      <form>
+  <div class="relative z-0 w-full mb-6 group">
+    <input
+      type="email"
+      name="floating_email"
+      id="floating_email"
+      class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+      placeholder=" "
+      required
+    />
+    <label
+      for="floating_email"
+      class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+    >
+      Mail
+    </label>
+  </div>
+
+  <div class="grid md:grid-cols-2 md:gap-6">
+    <div class="relative z-0 w-full mb-6 group">
+      <input
+        type="text"
+        name="floating_first_name"
+        id="floating_first_name"
+        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=" "
+        required
+      />
+      <label
+        for="floating_first_name"
+        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Nombre
+      </label>
+    </div>
+
+    <div class="relative z-0 w-full mb-6 group">
+      <input
+        type="tel"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        name="floating_phone"
+        id="floating_phone"
+        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        placeholder=" "
+        required
+      />
+      <label
+        for="floating_phone"
+        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        N° Celular
+      </label>
+    </div>
+  </div>
+
+  <div className="relative z-0 w-full mb-6 group">
+    <textarea
+      name="floating_description"
+      id="floating_description"
+      className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+      placeholder=" "
+      required
+    />
+    <label
+      htmlFor="floating_description"
+      className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+    >
+      Descripción
+    </label>
+  </div>
+
+  <button
+    type="submit"
+    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Submit
+  </button>
+</form>
+
+
+            <div className="flex justify-end mt-4">
+          <button
+            className="bg-blue-500 text-white py-2 px-4 rounded-lg"
+            onClick={handleCloseModal}
+          >
+            Cerrar
+          </button>
         </div>
       </div>
-    );
-  };
-  
-  export default Models;
+    </div>
+  </div>
+      )}
+    </div>
+  );
+};
+
+export default Models;
