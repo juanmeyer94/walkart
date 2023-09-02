3394b518d6840dba99df1032c2659dbaf65c372e
juanmeyer94 <juan_meyer@hotmail.com.ar>
2023-09-02 00:50:43 -0300

    fix makeitreal

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/home";
 import { Button } from "@/components/ui/button"
 import NavBar from "./components/NavBar/navbar"
 import Models from "./components/Models/Models";
+import MakeItReal from "./components/MakeItReal/MakeItReal";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <NavBar />
 
       <Routes>
+        <Route path="/personalizar" element={<MakeItReal/>} />
         <Route path="/" element={<Home />} />
         <Route path="/models" element={<Models />} />
         
