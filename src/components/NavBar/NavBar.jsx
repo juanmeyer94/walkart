b23d79bad179632c62917c2d85f57db6264002e6
IvoSoria <ivosoria@hotmail.com>
2023-09-04 13:25:39 -0300

    nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,22 +5,22 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="flex justify-between items-center text-3xl font-extrabold w-full font-mono bg-zinc-100 p-4">
-      <img src={logo} alt="Air White" className="h-[80px] w-[250px] rounded-[30%] lg:mx-auto lg:ml-0" />
+      <img src={logo} alt="Air White" className="h-[60px] w-[150px] rounded-[30%] lg:mx-auto lg:ml-0" />
       <div className="flex space-x-12 items-center">
         <Link to="/" >
-          <button className="hover:text-gray-500 text-2xl focus:outline-none" variant="ghost">INICIO</button>
+          <button className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-2xl font-medium px-5 py-2.5 text-center mr-2 mb-3 dark:text-white dark:focus:ring-gray-800 shadow-lg">INICIO</button>
         </Link>
         <Link to="/models" >
-          <button className="hover:text-gray-500 text-2xl focus:outline-none">CREACIONES</button>
+          <button className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-2xl font-medium px-5 py-2.5 text-center mr-2 mb-3 dark:text-white dark:focus:ring-gray-800 shadow-lg">CREACIONES</button>
         </Link>
         <Link to="/aboutus" >
-          <button className="hover:text-gray-500 text-2xl focus:outline-none">NOSOTROS</button>
+          <button className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-2xl font-medium px-5 py-2.5 text-center mr-2 mb-3 dark:text-white dark:focus:ring-gray-800 shadow-lg">NOSOTROS</button>
         </Link>
         <Link to="/contact" >
-          <button className="hover:text-gray-500 text-2xl focus:outline-none">CONTACTOS</button>
+          <button className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-2xl font-medium px-5 py-2.5 text-center mr-2 mb-3 dark:text-white dark:focus:ring-gray-800 shadow-lg">CONTACTOS</button>
         </Link>
         <Link to="/makeitreal">
-        <button className="bg-yellow-400 text-black text-2x1 px-6 py-2 rounded-full shadow-lg hover:bg-yellow-500 transition duration-300">MAKE IT REAL</button>
+          <button className="bg-yellow-400 hover:bg-yellow-500 text-black text-2xl px-6 py-2 rounded-full shadow-lg transition duration-300 mb-3">HAZLO REAL</button>
         </Link>
       </div>
     </div>
@@ -28,6 +28,7 @@ const NavBar = () => {
 }
 
 export default NavBar;
+
 
 
 
