15fae703cb46a1efa50f6fa62ed9f8377aad82c5
JulietaTomas <julietamtomas@gmail.com>
2023-09-04 10:54:12 -0300

    aboutus and update last project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home/home";
 import { Button } from "@/components/ui/button"
 import NavBar from "./components/NavBar/navbar"
 import Models from "./components/Models/Models";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/models" element={<Models />} />
+        <Route path="/aboutus" element={<AboutUs/>}/>
         
       </Routes>
     </div>
---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,16 @@
+import logo from "../Images/logo.jpg";
+
+const AboutUs = () => {
+  
+  return (
+    <div>
+     
+     <div className='flex items-center justify-center mt-8'><img className="rounded-xl" src={logo} alt="logo"/></div>
+
+<div ><p className="text-center mt-14 justify-center ml-[20%] mr-[20%] font-mono text-sans text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+
+</div>
+  )
+};
+
+export default AboutUs;
